3e4ff373ee5359a02326c9492ab923fa094bb9d5
soycracklitos <78566347+soycracklitos@users.noreply.github.com>
2021-07-16 18:09:23 -0500

    Add files via upload
    Ejercicios realizados en curso de python el día Viernes 16 de Julio del año 2021

---
+++ Temperaturas.py
@@ -0,0 +1,10 @@
+a=int(input("Ingrese la temperatura en Monterrey="))
+farenheit= a*(9/5)+32
+kelvin= a+273.15
+
+print("La temperatura en grados Farenheit de Monterrey es=", int(farenheit) )
+print("La temperatura en grados Kelvin de Monterrey es=", int(kelvin) )
+
+b=int(input("Ingrese la temperatura de Monterrey en farenheit="))
+celsius= (b-32)*(5/9)
+print("La temperatura en grados Celsius de Monterrey es=", (celsius) )
