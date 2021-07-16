fe6de688fe41e7d06dc89eacfc010dc09364c7b0
soycracklitos <78566347+soycracklitos@users.noreply.github.com>
2021-07-16 18:13:04 -0500

    Delete intento_formulaGeneral_pycourse.py

--- intento_formulaGeneral_pycourse.py
+++
@@ -1,8 +0,0 @@
-a=int(input("Ingrese el primer numero"))
-b=int(input("Ingrese el segundo numero"))
-c=int(input("Ingrese el tercer numero"))
-
-x= -b**(1/2)*(b**2-4*a*c)/2*a
-y= -b**(1/2)*(b**2-4*a*c)/2*a
-
-print("La respuesta de la formula general es=", int(x))
