2ce178baaeb5979fedab574b33840d739fef16c7
soycracklitos <78566347+soycracklitos@users.noreply.github.com>
2021-07-16 18:12:27 -0500

    Add files via upload

---
+++ Exercises_Friday17/Operaciones_pycourse.py
@@ -0,0 +1,17 @@
+a=int(input("Ingrese el numero a="))
+b=int(input("Ingrese el numero b="))
+x= int(a)*int(b)
+y=int(a)+int(b)
+z=int(a)/int(b)
+a=int(a)-int(b)
+print("Si tu operacion fuera una suma, la respuesta seria", x )
+print("Si tu operacion fuera una resta, la respuesta seria", a )
+print("Si tu operacion fuera una multiplicacion, la respuesta seria", y )
+print("Si tu operacion fuera una division , la respuesta seria", z )
+
+c=int(input("Ingrese el numero a elevar="))
+
+elevar= c**2
+
+print("Tu numero elevado a la segunda potencia da como resultado=", elevar)
+
